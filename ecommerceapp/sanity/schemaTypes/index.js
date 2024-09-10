02415924c55ab5e7abffeb0f13e54c3f26a5a60b
Deependra Singh <68831237+HERCULES005@users.noreply.github.com>
2024-09-10 14:22:10 +0530

    Completed schema and banner

--- ecommerceapp/sanity/schemaTypes/index.js
+++ ecommerceapp/sanity/schemaTypes/index.js
@@ -1,3 +1,11 @@
-export const schema = {
-  types: [],
-}
+import createSchema from 'part:@sanity/base/schema-creator';
+import schemaTypes from 'all:part:@sanity/base/schema-type';
+
+import product from './product';
+import banner from './banner';
+
+export default createSchema({
+  name: 'default',
+  types: schemaTypes.concat([ product, banner ]),
+})
+  
