818ee7eb8ab78ca235838fc56d578ae871b0e7d1
Deependra Singh <68831237+HERCULES005@users.noreply.github.com>
2024-12-15 00:38:57 +0530

    Routes linked

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {BrowserRouter} from 'react-dom'
+//Try react router or react-router-dom if not working 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+  
+    <App/>
+  
+  </BrowserRouter>,
 )
